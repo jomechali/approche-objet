6c08265de0d338930a4f2cbe6770e5e6d5253485
Joseph MECHALI <mechali.joseph@gmail.com>
2022-06-10 16:33:28 +0200

    tp 5 : on constate que la console n affiche rien de lisible!

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,29 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+
+	private int numeroDuCompte;
+	private int solde;
+	
+	public Compte(int numeroDuCompte, int solde) {
+		this.numeroDuCompte = numeroDuCompte;
+		this.solde = solde;
+	}
+
+	public int getNumeroDuCompte() {
+		return numeroDuCompte;
+	}
+
+	public void setNumeroDuCompte(int numeroDuCompte) {
+		this.numeroDuCompte = numeroDuCompte;
+	}
+
+	public int getSolde() {
+		return solde;
+	}
+
+	public void setSolde(int solde) {
+		this.solde = solde;
+	}
+	
+}
---
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,12 @@
+package fr.diginamic.banque.entites;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+
+		Compte compteTest = new Compte(12345, 1000000);
+		
+		System.out.println(compteTest);
+	}
+
+}
