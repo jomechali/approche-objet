97c1416782c8be004023a2faa839af55aacdf696
Joseph MECHALI <mechali.joseph@gmail.com>
2022-06-23 10:51:35 +0200

    TP Apprendre a utiliser equals, avec Maven et appache common

--- src/fichier/Ville.java
+++ src/fichier/Ville.java
@@ -1,11 +1,14 @@
 package fichier;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
 public class Ville {
 
 	private String nom;
 	private String codeDepartement;
 	private String nomRegion;
 	private int popTotale;
+
 	/**
 	 * @param nom
 	 * @param codeDepartement
@@ -19,7 +22,7 @@ public class Ville {
 		this.nomRegion = nomRegion;
 		this.popTotale = popTotale;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Ville [nom=" + nom + ", codeDepartement=" + codeDepartement + ", nomRegion=" + nomRegion
@@ -33,60 +36,72 @@ public class Ville {
 			return false;
 		}
 		Ville ville = (Ville) obj;
-		
-		return (nom.equals(ville.nom) && codeDepartement.equals(ville.codeDepartement) 
-				&& nomRegion.equals(ville.nomRegion) && popTotale == ville.popTotale);
+
+		/*
+		 * return (nom.equals(ville.nom) &&
+		 * codeDepartement.equals(ville.codeDepartement) &&
+		 * nomRegion.equals(ville.nomRegion) && popTotale == ville.popTotale);
+		 */
+		return new EqualsBuilder().append(nom, ville.nom).append(codeDepartement, ville.codeDepartement)
+				.append(nomRegion, ville.nomRegion).append(popTotale, ville.popTotale).isEquals();
 	}
+
 	/**
 	 * @return the relevant data in the writing format
 	 */
 	public String getData() {
 		return String.format("%s;%s;%s;%d", nom, codeDepartement, nomRegion, popTotale);
 	}
-	
-	
+
 	/**
 	 * @return the nom
 	 */
 	public String getNom() {
 		return nom;
 	}
+
 	/**
 	 * @return the codeDepartement
 	 */
 	public String getCodeDepartement() {
 		return codeDepartement;
 	}
+
 	/**
 	 * @return the nomRegion
 	 */
 	public String getNomRegion() {
 		return nomRegion;
 	}
+
 	/**
 	 * @return the popTotale
 	 */
 	public int getPopTotale() {
 		return popTotale;
 	}
+
 	/**
 	 * @param nom the nom to set
 	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
 	/**
 	 * @param codeDepartement the codeDepartement to set
 	 */
 	public void setCodeDepartement(String codeDepartement) {
 		this.codeDepartement = codeDepartement;
 	}
+
 	/**
 	 * @param nomRegion the nomRegion to set
 	 */
 	public void setNomRegion(String nomRegion) {
 		this.nomRegion = nomRegion;
 	}
+
 	/**
 	 * @param popTotale the popTotale to set
 	 */
@@ -94,7 +109,4 @@ public class Ville {
 		this.popTotale = popTotale;
 	}
 
-
-	
-	
 }
